db29d6cc2df48b8a6b22fea421cc1bd9116f6729
fcoedilson <fcoedilson@gmail.com>
2014-09-21 23:40:21 -0300

    artefatos iniciais do projeto

---
+++ main.lua
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+--  local myTextObject = display.newText( "Hello World!", 160, 240, "Arial", 60 )
+-- myTextObject:setFillColor( 1, 0, 0 )
+
+local background = display.newImage("images/desenho.png",60,120,true)
+
+
+
+
